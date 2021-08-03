459d094d2c48fe2df847ec78329d8f8fe1c37ab8
Amit <amideo60@gmail.com>
2021-08-03 15:44:18 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -16,7 +16,9 @@ export const Footer = () => {
           In our hustle bustle routine, we use to forget some of our important works which was supposed to complete. This web-app will help you to pen-down all your to-do list and allows you to update accordingly. <br/><br/>
 
            copyright &copy; Designed and developed by Amit Deo <br/>
-           follow me on github @ amitdeo60
+           
+
+           
 
         </footer>
     )
